9cf96a43d7bddff325ac248aba0667dbb4812324
Ahmad <wisamfaithful@gmail.com>
2015-02-03 14:21:46 -0500

    refactored problem 46

--- 46/solution.rb
+++ 46/solution.rb
@@ -1,33 +1,53 @@
 t0 = Time.now
 require 'prime'
 
-def squares_times_two(num)
-  squares = []
-  (1..Math.sqrt(num)).each do |n|
-    squares << (n*n) * 2
-  end
-  squares
-end
+class Problem46
 
-def conjecture?(num)
-	primes = Prime.take(num)
-	primes.each do |p|
-		squares_times_two(num).each do |s|
-			return true if p + s == num
+	def initialize
+		@primes_array = [ 2 ]
+	end
+
+	def squares_times_two(number)
+	  squares = []
+	  (1..Math.sqrt(number)).each do |n|
+	    squares << (n*n) * 2
+	  end
+	  squares
+	end
+
+	def primes(addPrime)
+	  (@primes_array.last..addPrime).each do |p|
+	  	@primes_array << p if Prime.prime?(p)
+	  end
+	  @primes_array
+	end
+
+	def conjecture?(num)
+		primes(num).each do |prime|
+			squares_times_two(num).each do |s|
+				return true if prime + s == num
+			end
+		end
+		false
+	end
+
+
+	def find
+		num =33
+		while true 
+			if num%2 != 0 && !Prime.prime?(num) && !conjecture?(num) 
+				puts num
+				break
+			end
+		  num+=1
 		end
 	end
-	false
 end
 
-num =33
-while true 
-	if num%2 != 0 && !Prime.prime?(num) && !conjecture?(num) 
-		puts num
-		break
-	end
-  num+=1
-end
+problem = Problem46.new
+problem.find
 
 p Time.now - t0
+
 
 
